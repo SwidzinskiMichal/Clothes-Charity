6b046935a5d36aa25ca9d9fe54ec28990335e2f3
Michał Świdziński <michalswidzinski01@gmail.com>
2023-04-17 17:19:08 +0200

    Institution model

--- charity/models.py
+++ charity/models.py
@@ -3,3 +3,15 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
+
+
+choices = [('fundacja', 'Fundacja'),
+           ('organizacja_pozarzadowa', 'Organizacja pozarządowa'),
+           ('zbiorka_lokalna', 'Zbiórka lokalna')]
+
+
+class Institution(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    type = models.CharField(max_length=30, choices=choices, default= 'fundacja')
+    categories = models.ManyToManyField(Category)
