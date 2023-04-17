5878a6f1c04c853bfa7f698d550faba3e2210c7a
Michał Świdziński <michalswidzinski01@gmail.com>
2023-04-17 17:33:47 +0200

    Dontaions models

--- charity/models.py
+++ charity/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Category(models.Model):
@@ -15,3 +16,17 @@ class Institution(models.Model):
     description = models.TextField()
     type = models.CharField(max_length=30, choices=choices, default= 'fundacja')
     categories = models.ManyToManyField(Category)
+
+
+class Donation(models.Model):
+    quantity = models.IntegerField()
+    categories = models.ManyToManyField(Category)
+    institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
+    address = models.CharField(max_length=250)
+    phone_number = models.CharField(max_length=50)
+    city = models.CharField(max_length=250)
+    zip_code = models.CharField(max_length=50)
+    pick_up_date = models.DateField()
+    pick_up_time = models.DateField()
+    pick_up_comment = models.TextField()
+    user = models.ForeignKey(User, default=None, on_delete=models.CASCADE)
