229dfe9ce7cb908b468d465a2b0d68917963fc42
Michał Świdziński <michalswidzinski01@gmail.com>
2023-04-22 16:07:16 +0200

    Registration

--- charity/views.py
+++ charity/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
 from . import models
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 def landingpage(request):
     return render(request, "charity/index.html", {'donations_all': models.Donation.objects.all().count(),
@@ -18,4 +20,24 @@ def login(request):
 
 
 def register(request):
-    return render(request, "charity/register.html")
+    if request.method == 'POST':
+        # Extract form data
+        name = request.POST['name']
+        surname = request.POST['surname']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        # Create new user
+        user = User.objects.create_user(
+            username=email,
+            email=email,
+            password=password,
+            first_name=name,
+            last_name=surname
+        )
+
+        # Redirect to login page
+        return redirect('/login/')
+    else:
+        return render(request, 'charity/register.html')
--- config/settings.py
+++ config/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
