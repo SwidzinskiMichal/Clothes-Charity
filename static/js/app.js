42a3df3fc0a1aff7b9076a816437f878f0c199b8
Michał Świdziński <michalswidzinski01@gmail.com>
2023-04-27 17:09:23 +0200

    Step three

--- static/js/app.js
+++ static/js/app.js
@@ -253,3 +253,13 @@ document.addEventListener("DOMContentLoaded", function() {
     new FormSteps(form);
   }
 });
+
+const categoryBtn = document.getElementById('category-btn');
+categoryBtn.addEventListener('click', function() {
+  const checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
+  const values = [];
+  checkboxes.forEach(function(checkbox) {
+    values.push(checkbox.value);
+  });
+  console.log(values);
+});
